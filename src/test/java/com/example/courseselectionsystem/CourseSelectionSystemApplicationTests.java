1c2aa6dba3bde3ba75e180074dba45c5e37f0ec6
Song <song71017@163.com>
2026-05-15 16:15:50 +0800

    Initial commit

---
+++ src/test/java/com/example/courseselectionsystem/CourseSelectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseselectionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSelectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
